9e3237f0fe5c7f9b4b9b0e84bd45c6341fcf87ec
John Mumm <jtfmumm@gmail.com>
2025-10-16 20:07:31 +0100

    More logging

--- src/keyhive/keyhive.ts
+++ src/keyhive/keyhive.ts
@@ -169,7 +169,7 @@ export async function loadOrCreateKeyhive(
         console.log("[Adapter] Attempting to load Keyhive archive");
         let store = CiphertextStore.newInMemory();
         const chunk_count = keyhiveArchiveChunks.length;
-        console.log(`[Adapter] Ingesting archive from storage (1 of ${chunk_count})`);
+        console.log(`[Adapter] Ingesting archive from storage (1 of ${chunk_count}). Hash: ${firstChunk.key[2]}`);
         const kh = await firstArchive.tryToKeyhive(store, signer, event_handler);
         for (let idx = 1; idx < keyhiveArchiveChunks.length; idx++) {
           const chunk = keyhiveArchiveChunks[idx];
